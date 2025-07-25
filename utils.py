f96054540f349a0c76b28a0b4b41b44f4514229f
Valentin Gebhart <valentin.gebhart@usys.ethz.ch>
2025-07-25 10:09:13 +0200

    convert frequency to probability util function

--- utils.py
+++ utils.py
@@ -34,3 +34,29 @@ def freq_to_prob(frequency, coincidence_fraction=1 / 12):
 def prob_to_freq(probability, time_unit="year", coincidence_fraction=1 / 12):
     # check value range?
     return -np.log(1 - probability) / coincidence_fraction
+
+def prob_from_exceedance_frequency(exceedance_frequency, coincidence_fraction=1 / 12):
+    """
+    Convert exceedance frequency to probability.
+
+    Parameters:
+    exceedance_frequency (float or array-like): The exceedance frequency.
+    time_unit (str): The time unit for the return period (default is "year").
+    coincidence_fraction (float): Fraction of the year that the event coincides with (default is 1/12).
+
+    Returns:
+    float or np.ndarray: The probability corresponding to the exceedance frequency.
+    """
+    # convert time unit to coincidence window
+    exceedance_frequency *= coincidence_fraction
+    exceedance_frequency = np.insert(exceedance_frequency, exceedance_frequency.shape[-1], 0., axis=-1)
+    
+    # compute frequencies
+    freqs = np.flip(np.diff(np.flip(exceedance_frequency, axis=-1)), axis=-1)
+
+    # compute probabilities (P (event_i) = P (at least one event_i)))
+    probabilities = 1 - np.exp(- freqs)
+    # add probability for nothing happening
+    probabilities = np.insert(probabilities, 0, 1. - probabilities.sum(axis=-1), axis=-1)
+
+    return probabilities
