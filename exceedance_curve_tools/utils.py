1d674c7b14ead989998c93f6a4927a74adde8694
Valentin Gebhart <valentin.gebhart@usys.ethz.ch>
2025-10-03 14:00:10 +0200

    update tutorial: combine curves

--- exceedance_curve_tools/utils.py
+++ exceedance_curve_tools/utils.py
@@ -3,7 +3,7 @@
 import numpy as np
 from scipy.stats import norm
 
-
+### rounding functions
 def round_to_array(obj, array):
     """
     Round the elements of `obj` to the nearest value in `arr`.
@@ -27,7 +27,7 @@ def round_to_array(obj, array):
     # Replace each entry in object with the closest entry from array
     return array[indices]
 
-
+### frequency/probability conversion functions
 def prob_from_exceedance_frequency(exceedance_frequency, coincidence_fraction=1 / 12):
     """
     Convert exceedance frequency to probability.
@@ -124,10 +124,11 @@ def frequency_from_exceedance_frequency(exceedance_frequency):
         axis=-1,
     )
 
-
+### sampling functions
 def get_correlated_quantiles(d, correlation_factor, n_samples):
     """sample correlated quantiles"""
     # create covariance matrix
+    correlation_factor = float(correlation_factor)
     if d == 1 or correlation_factor == 0:
         return np.random.random(size=(n_samples, d))
 
@@ -149,15 +150,79 @@ def get_correlated_quantiles(d, correlation_factor, n_samples):
     # transform normal samples to uniform(0, 1)
     return norm.cdf(normal_samples)
 
+# def check_if_corr_valid_given_marginals(p, corr):
+#     """check if given correlation matrix is valid given marginal probabilities"""
+#     n = len(p)
 
-# def sort_two_arrays_by_first(arr1, arr2, ascending=True):
-#     if ascending:
-#         order = np.argsort(arr1)
+#     # Compute thresholds for each variable
+#     thresholds = norm.ppf(1 - p)
+
+#     # Compute feasible correlation bounds
+#     lower_bounds = np.zeros((n, n))
+#     upper_bounds = np.zeros((n, n))
+#     for i in range(n):
+#         for j in range(n):
+#             if i == j:
+#                 lower_bounds[i, j] = 1.0
+#                 upper_bounds[i, j] = 1.0
+#             else:
+#                 lower_bounds[i, j] = (
+#                     (np.exp(-thresholds[i] - thresholds[j]) - (1 - p[i]) * (1 - p[j]))
+#                     / np.sqrt(p[i] * (1 - p[i]) * p[j] * (1 - p[j]))
+#                 )
+#                 upper_bounds[i, j] = (
+#                     (min(p[i], p[j]) - (1 - p[i]) * (1 - p[j]))
+#                     / np.sqrt(p[i] * (1 - p[i]) * p[j] * (1 - p[j]))
+#                 )
+
+#     # Check if all correlations are within bounds
+#     if np.all((corr >= lower_bounds) & (corr <= upper_bounds)):
+#         return True
 #     else:
-#         order = np.argsort(arr1)[::-1]
-#     return arr1[order], arr2[order]
+#         return False
 
+# def sample_correlated_bernoulli(p, corr, n_samples):
+#     """
+#     Sample n Bernoulli variables with given means and target correlation matrix using a Gaussian copula.
 
+#     Parameters
+#     ----------
+#     p : array-like, shape (n,)
+#         Marginal probabilities for each variable.
+#     corr : array-like, shape (n, n)
+#         Target correlation matrix (must be symmetric, positive semi-definite, 1 on diagonal).
+#     n_samples : int
+#         Number of samples to generate.
+
+#     Returns
+#     -------
+#     samples : ndarray, shape (n_samples, n)
+#         Sampled Bernoulli variables (0 or 1).
+#     """
+#     p = np.asarray(p)
+#     corr = np.asarray(corr)
+#     n = len(p)
+
+#     # Check correlation matrix
+#     if corr.shape != (n, n):
+#         raise ValueError("corr must be of shape (n, n)")
+#     if not np.allclose(np.diag(corr), 1):
+#         raise ValueError("Diagonal of corr must be 1")
+#     if not np.allclose(corr, corr.T):
+#         raise ValueError("corr must be symmetric")
+
+#     # Compute thresholds for each variable
+#     thresholds = norm.ppf(1 - p)
+
+#     # Sample from multivariate normal
+#     mean = np.zeros(n)
+#     mvn_samples = np.random.multivariate_normal(mean, corr, size=n_samples)
+
+#     # Apply thresholds to get Bernoulli samples
+#     samples = (mvn_samples < thresholds).astype(int)
+#     return samples
+
+### other utils
 def fill_edges(a):
     """fill initial (final) NaN values along axis 1 with first (final)
     non-NaN value. NaNs in between non-NaNs are not filled."""
